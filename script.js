9e3c75b3bc2602763a5a24ef54a658dd8f6f7e60
EddieSAWADOGO <sawadogoeddie05@gmail.com>
2026-05-31 10:41:02 +0000

    Debut de la premiere version 1.1

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ function scrollToTop() {
 
 // Fade-in animations on scroll
 const observerOptions = {
-    threshold: 0.15,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0.1,
+    rootMargin: '0px'
 };
 
 const observer = new IntersectionObserver(function(entries) {
